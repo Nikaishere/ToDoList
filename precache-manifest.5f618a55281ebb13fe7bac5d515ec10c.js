db773324c5536aa7432ce2d595d38579ac7181d3
Veronica Rivera <veronikafranck@hotmail.com>
2023-05-01 14:52:09 +0200

    Updates

---
+++ precache-manifest.5f618a55281ebb13fe7bac5d515ec10c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdced8e7951927d6ee4c87d44be02c1",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "2dd71fbc6ce90cdca2c6",
+    "url": "/ToDoList/static/css/main.57d572be.chunk.css"
+  },
+  {
+    "revision": "51f54a6b8606b3be6c6e",
+    "url": "/ToDoList/static/js/2.8a43d448.chunk.js"
+  },
+  {
+    "revision": "2dd71fbc6ce90cdca2c6",
+    "url": "/ToDoList/static/js/main.6f939f88.chunk.js"
+  },
+  {
+    "revision": "29b5eb60005077ac4cdd",
+    "url": "/ToDoList/static/js/runtime-main.41b1c708.js"
+  }
+]);
